60f4f9186bc8a4fd993566cf7a53d885c130e9e8
tringcooler <tringcooler@users.noreply.github.com>
2018-01-18 02:35:31 +0800

    standard commit

--- src/core/action.js
+++ src/core/action.js
@@ -7,6 +7,7 @@ define(function(require) {
     function action(objs) {
         _super.call(this);
         this.objs = objs;
+        this._breaking = false;
     }
     
     action.prototype.SETID('#ACTION');
@@ -14,19 +15,22 @@ define(function(require) {
         console.log('action init', proto.ID);
     };
     
-    ction.prototype.PRIO_ACT = 100;
+    action.prototype.PRIO_ACT = 100;
     
     action.prototype._emit_to_obj = function(node) {
-        var objs = this.objs;
+        var self = this
         var node_mp = new (require('core/util').multi_pool)(node);
-        for(var i = 0; i < objs.length; i++) {
-            var obj = objs[i];
+        for(var i = 0; i < self.objs.length; i++) {
+            var obj = self.objs[i];
             node_mp.foreach([i, '*'], function(cb, path) {
                 var sk_id = path[1];
                 obj.foreach_skill(sk_id, null, function(skid, srcid, sk) {
-                    cb.call(sk, obj, objs);
+                    if(!self._is_break()) {
+                        cb.call(sk, self, obj, self.objs);
+                    }
                 });
             });
+            if(self._is_break()) break;
         }
     };
     
@@ -37,13 +41,31 @@ define(function(require) {
             var prio = prios[i];
             if(prio >= this.PRIO_ACT) break;
             this._emit_to_obj(node[prio]);
+            if(this._is_break()) return this._break_done();
         }
         if(this.effect) this.effect();
+        if(this._is_break()) return this._break_done();
         for(; i < prios.length; i++) {
             var prio = prios[i];
             this._emit_to_obj(node[prio]);
+            if(this._is_break()) return this._break_done();
         }
     };
+    
+    action.prototype._is_break = function() {
+        return this._breaking;
+    };
+    
+    action.prototype._break_done = function(v) {
+        this._breaking = false;
+        return v;
+    };
+    
+    action.prototype.break = function() {
+        this._breaking = true;
+    };
+    
+    action.prototype.effect = function() {};
     
     return action;
     
