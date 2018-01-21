e3f5c4b7216d997a6374cb3606061dc05745f99d
tringcooler <tringcooler@users.noreply.github.com>
2018-01-21 19:22:42 +0800

    build config

---
+++ src/build.js
@@ -0,0 +1,13 @@
+({
+    baseUrl: '.',
+    paths: {
+        jquery: '../lib/jquery-1.12.4.min',
+        core: 'core',
+        comp: 'comp',
+        action: 'comp/action',
+        skill: 'comp/skill',
+        entity: 'comp/entity',
+    },
+    name: "main",
+    out: "../build/wodlar_build.js"
+})
